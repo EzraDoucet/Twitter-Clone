4fbc386264f601eccc66c6d1be6ecba014b03f42
Ezra <ezra.doucet@gmail.com>
2017-12-11 09:29:15 -0600

    Add ability to create tweets.  Setup Tweet Show view.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,33 @@
 class TweetsController < ApplicationController
+ before_action :authenticate_user!
+ before_action :set_tweet, only: []
+
   def index
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    if @tweet.save
+      redirect_to @tweet, notice: "That Tweet was Saved!"
+    else
+      render :new, notice: 'Sorry, couldn\'t keep that guy around'
+    end
+
   end
 
   def edit
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
-
-  get 'tweets/show'
-
-  get 'tweets/new'
-
-  get 'tweets/edit'
+  resources :tweets
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'tweets#index'
+
 end
