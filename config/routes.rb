494cbd76d5bfcbbfe7e4d85d8f1026d656942856
Ezra <ezra.doucet@gmail.com>
2017-12-12 10:55:17 -0600

    Add ability to like and unlike tweets on Show View.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get 'profiles' => 'profiles#index'
   get 'profiles/:id' => 'profiles#show', as: :profile
 
-  resources :tweets
+  resources :tweets do
+    resource :likes
+  end
   resources :relationships, only: [:create, :destroy]
-  resources :likes, only: [:create, :destroy]
+  # resources :likes, only: [:create, :destroy]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
