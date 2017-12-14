5fa1041f5813f063cd779e3cf3fcb6fe4c3dc7e1
Ezra <ezra.doucet@gmail.com>
2017-12-13 19:14:56 -0600

    Make Edit and Delete only show up if user owns tweet.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,5 +5,6 @@ class ProfilesController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @followers = Relationship.where(friend_id: @user.id).map {|user| User.find(user.user_id)}
   end
 end
